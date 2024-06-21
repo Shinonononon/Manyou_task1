b7bcc667ab63d232033c7834232d3302d05a5239
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-21 15:13:19 +0900

    要所修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.new(title: '', content: 'タスクの内容')
+        expect(task).not_to be_valid
+        #expect(task.errors[:title]).to include("Title can’t be blank")
+      end
+    end
+
+    context 'タスクの説明が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.new(title: 'タスクのタイトル', content: '')
+        expect(task).not_to be_valid
+        #expect(task.errors[:content]).to include("Content can’t be blank")
+      end
+    end
+
+    context 'タスクのタイトルと説明に値が入っている場合' do
+      it 'タスクを登録できる' do
+        task = Task.new(title: 'タスクのタイトル', content: 'タスクの内容')
+        expect(task).to be_valid
+      end
+    end
+  end
 end
