e7929f33b0d07a7192818a67c9abbf2ef4eb90c8
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-23 02:15:49 +0900

    テスト改良

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -9,4 +9,8 @@ FactoryBot.define do
     title { 'メール送信' }
     content { '顧客へ営業のメールを送る。' }
   end
+  factory :third_task, class: Task do
+    title { '会議室予約' }
+    content { '明日の会議のための部屋を予約する。' }
+  end
 end
