54da6cd389063c65ea31c0e1fc15b3788fd1147c
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-25 00:59:26 +0900

    カラム追加、enum設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :deadline_on, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+
+  enum priority: { low: 0, midium: 1, high: 2 }
+  enum status: { NotStarted: 0, InProgress: 1, Completed: 2 }
 end
