8de078b9028396638393dd1f19008d7a99180523
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-25 13:59:47 +0900

    追加カラム分view修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ApplicationRecord
+  enum priority: { low: 0, medium: 1, high: 2 }
+  enum status: { NotStarted: 0, InProgress: 1, Completed: 2 }
+
   validates :title, presence: true
   validates :content, presence: true
   validates :deadline_on, presence: true
   validates :priority, presence: true
   validates :status, presence: true
 
-  enum priority: { low: 0, midium: 1, high: 2 }
-  enum status: { NotStarted: 0, InProgress: 1, Completed: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_24_153206) do
+ActiveRecord::Schema.define(version: 2024_06_25_024829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2024_06_24_153206) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.date "deadline_on", default: -> { "CURRENT_DATE" }, null: false
+    t.date "deadline_on", default: "2024-06-25", null: false
     t.integer "priority", default: 0, null: false
     t.integer "status", default: 0, null: false
   end
