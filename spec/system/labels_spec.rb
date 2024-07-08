a951e5517181d7c58653f85b40686678e735e841
Shinonononon <h1karu.sn0928@gmail.com>
2024-07-08 15:36:46 +0900

    できた！

--- spec/system/labels_spec.rb
+++ spec/system/labels_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
   RSpec.describe 'ラベル管理機能', type: :system do
     describe '登録機能' do
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:first_label) { FactoryBot.build(:label, user: user) }
+      let!(:second_label) { FactoryBot.build(:second_label, user: user) }
+      before do
+        visit labels_path
+      end
       context 'ラベルを登録した場合' do
         it '登録したラベルが表示される' do
-          label = Label.new(name: 'aaa', user: create(:user))
-          redirect_to labels_path
           expect(page).not_to have_content 'aaa'
         end
       end
@@ -12,9 +16,6 @@ require 'rails_helper'
     describe '一覧表示機能' do
       context '一覧画面に遷移した場合' do
         it '登録済みのラベル一覧が表示される' do
-          label = Label.new(name: 'aaa', user: create(:user))
-          label2 =Label.new(name: 'bbb', user: (:user))
-          redirect_to labels_path
           expect(page).not_to have_content 'aaa'
           expect(page).not_to have_content 'bbb'
         end
